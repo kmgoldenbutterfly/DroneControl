9b52ece0d2b006bee60166831459defe01cbea84
pgilmorekm <pdgilmore@outlook.com>
2015-08-22 17:28:07 -0700

    Add waypoint capability
    Moving objects can move between a a sequence of waypoints.

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MovingObject))]
+public class Waypoint : MonoBehaviour
+{
+    private MovingObject movingObject;
+    private int index = 0;
+    public Vector3[] wayPoints;
+    public bool loop = false;
+
+    void Start()
+    {
+        movingObject = GetComponent<MovingObject>();
+    }
+
+    void Update()
+    {
+        if (movingObject.moveOption == MovingObject.MoveOption.Arrived && index < wayPoints.Length)
+        {
+            movingObject.setMovement = true;
+            movingObject.moveOption = MovingObject.MoveOption.MoveToLocation;
+            movingObject.moveToLocation = wayPoints[index];
+            index = loop ? (index + 1) % wayPoints.Length : index +1;
+        }
+    }
+}
